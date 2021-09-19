41c0e9ec97da175a015c4ce6a1288257bd66e153
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2021-09-19 11:31:31 +0800

    [更新版本号][版本号更新到1.3.8]

--- sln_viznas_iot_elock_oobe/board/board_rt106f_elock.h
+++ sln_viznas_iot_elock_oobe/board/board_rt106f_elock.h
@@ -497,8 +497,8 @@ extern "C" {
 #define OTA_SUPPORT		0
 #define	FFD_SUPPORT		0
 
-#define  PROJECT_VERSION  "W8_106F_V1.3.7"
-#define  FIRMWARE_VERSION  "FW_W8_9c6bb0_20210918_V1.3.7"
+#define  PROJECT_VERSION  "W8_106F_V1.3.8"
+#define  FIRMWARE_VERSION  "FW_W8_c12db8_20210919_V1.3.8"
 
 /*******************************************************************************
  * API
