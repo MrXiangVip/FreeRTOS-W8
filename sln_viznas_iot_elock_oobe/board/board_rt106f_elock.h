e2d7c562a716e7a7d7b0bec05b353481137a3a71
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2022-10-26 14:43:10 +0800

    [R60][版本号更新到119]

--- sln_viznas_iot_elock_oobe/board/board_rt106f_elock.h
+++ sln_viznas_iot_elock_oobe/board/board_rt106f_elock.h
@@ -497,9 +497,9 @@ extern "C" {
 #define OTA_SUPPORT		0
 #define	FFD_SUPPORT		0
 
-#define  SYS_VERSION		"1.1.8"
-#define  PROJECT_VERSION    "R60_106F_V1.1.8"
-#define  FIRMWARE_VERSION   "FW_R60_093d07_20221024_V1.1.8"
+#define  SYS_VERSION		"1.1.9"
+#define  PROJECT_VERSION    "R60_106F_V1.1.9"
+#define  FIRMWARE_VERSION   "FW_R60_093d07_20221026_V1.1.9"
 
 /*******************************************************************************
  * API
